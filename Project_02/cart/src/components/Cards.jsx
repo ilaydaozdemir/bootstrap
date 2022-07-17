170d09f2aab24a55b8252dbfcc04bd1679c1d6a2
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-07-17 19:08:33 +0300

    Add props for components

--- Project_02/cart/src/components/Cards.jsx
+++ Project_02/cart/src/components/Cards.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import "./Cards.css";
 
-const Cards = () => {
+const Cards = (props) => {
   return (
     <>
       <div className="col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-4">
         <div className="card h-100 shadow">
-          <img src="" alt="" className="card-img-top img-fluid" />
+          <img src={props.img} alt="" className="card-img-top img-fluid" />
           <div className="card-body">
-            <h5 className="card-title">Lorem, ipsum dolor.</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet.</p>
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.desc}</p>
             <button className="btn btn-success">Add to Cart</button>
           </div>
         </div>
--- Project_02/cart/src/pages/Home.jsx
+++ Project_02/cart/src/pages/Home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import Data from "../backend/Data";
+
 import Cards from "../components/Cards";
 import "./Home.css";
+import data from "../backend/Data";
 const Home = () => {
   return (
     <div className="container-fluid">
       <h3 className="text-center mt-5 text-uppercase">Shop Page</h3>
       <div className="container py-4">
         <div className="row">
-          <Cards />
+          {Data.products.map((item, index) => {
+            return (
+              <Cards
+                img={item.img}
+                title={item.title}
+                desc={item.decs}
+                price={item.price}
+                key={index}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
