251c189361e0861b4ebeeae341c60e920c9225d8
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-07-17 19:08:06 +0300

    Add Data

---
+++ Project_02/cart/src/backend/Data.js
@@ -0,0 +1,35 @@
+import img from "./images/1.jpeg";
+
+const data = {
+  products: [
+    {
+      id: 1,
+      img: img,
+      title: "Product One",
+      decs: "Lorem, ipsum dolor.",
+      price: 80,
+    },
+    {
+      id: 2,
+      img: img,
+      title: "Product Two",
+      decs: "Lorem, ipsum dolor.",
+      price: 85,
+    },
+    {
+      id: 3,
+      img: img,
+      title: "Product Three",
+      decs: "Lorem, ipsum dolor.",
+      price: 65,
+    },
+    {
+      id: 4,
+      img: img,
+      title: "Product Four",
+      decs: "Lorem, ipsum dolor.",
+      price: 70,
+    },
+  ],
+};
+export default data;
