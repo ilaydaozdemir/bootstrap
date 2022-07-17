7a6137f32ad0b1b6cc0c60849027b90847cdaa1a
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-07-17 19:18:11 +0300

    Add style

--- Project_02/cart/src/components/Cards.jsx
+++ Project_02/cart/src/components/Cards.jsx
@@ -9,9 +9,19 @@ const Cards = (props) => {
         <div className="card h-100 shadow">
           <img src={props.img} alt="" className="card-img-top img-fluid" />
           <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">{props.desc}</p>
-            <button className="btn btn-success">Add to Cart</button>
+            <div className="d-flex justify-content-between">
+              <h5 className="card-title">{props.title}</h5>
+              <span className="fw-bolder">${props.price}</span>
+            </div>
+            <div className="">
+              {" "}
+              <p className="card-text">{props.desc}</p>
+            </div>
+            <div className=" d-grid justify-content-end mt-4">
+              <button className="btn btn-sm btn-outline-success">
+                Add to Cart
+              </button>
+            </div>
           </div>
         </div>
       </div>
